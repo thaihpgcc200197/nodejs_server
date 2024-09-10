229d32aaa7444c92cd71bc0e5e7eb42b6789eec8
thai <106147688+thaihpgcc200197@users.noreply.github.com>
2024-09-10 10:56:07 +0700

    update

---
+++ src/env/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  CONNECTION_STRING: "mongodb://localhost:27017/hung-bid",
+  APP_MAIL: "",
+  APP_MAIL_PASS: "",
+  JWT_SECRET_KEY: "9bcc6413-1cab-4539-8830-a7116d787c7b",
+  TOKEN_IMG_CLOUD: "",
+  BYTE_SCALE_ACCOUNT_ID: "12a1z63",
+  BYTE_SCALE_PUBLIC_KEY: "public_12a1z63CSpJZMDeJ9zALJNiUv1Qt",
+  BYTE_SCALE_SECRET_KEY: "secret_12a1z63D4nKY4SZsCapgWDwTkyLf",
+};
