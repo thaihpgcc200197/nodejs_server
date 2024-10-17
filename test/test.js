48726a134dab54b40511a8ab800193425dbe569d
thai <106147688+thaihpgcc200197@users.noreply.github.com>
2024-10-17 15:50:23 +0700

    delete file

---
+++ test/test.js
@@ -0,0 +1,23 @@
+// console.log(new Date("2024/12/12 15:15:15"))
+// console.log(new Date("12/12/2024 15:15:15"))
+
+// console.log(new Date("23-12-2023 15:15:15"))
+// console.log();
+const temp = new Date("12/25/2023 15:16:17:18");
+const test = new Date();
+console.log(temp);
+
+console.log(test);
+if (temp<test) console.log('ok');
+
+
+// console.log(temp);
+
+// console.log(temp.getDate());
+// console.log(temp.getMonth());
+// console.log(temp.getHours());
+// console.log(temp.getFullYear());
+// console.log(temp.getMinutes());
+// console.log(temp.getSeconds());
+// console.log(temp.getMilliseconds());
+
